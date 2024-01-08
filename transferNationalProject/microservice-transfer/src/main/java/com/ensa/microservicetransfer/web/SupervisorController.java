a27da704c8727d45750bd23cad250a1fbbc077ea
LAMIA1OSL <lamiaoussalem@gmail.com>
2024-01-08 21:26:55 +0100

    initial commit transfert national

---
+++ transferNationalProject/microservice-transfer/src/main/java/com/ensa/microservicetransfer/web/SupervisorController.java
@@ -0,0 +1,37 @@
+package com.ensa.microservicetransfer.web;
+
+import com.ensa.microservicetransfer.entities.Supervisor;
+import com.ensa.microservicetransfer.enums.SupervisorRole;
+import com.ensa.microservicetransfer.services.SupervisorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequestMapping("/supervisors")
+@RestController
+public class SupervisorController {
+    private SupervisorService supervisorService;
+
+    @Autowired
+    public void setSupervisorService(SupervisorService supervisorService) {
+        this.supervisorService = supervisorService;
+    }
+
+    @PostMapping("/create")
+    public Supervisor create(@RequestBody Supervisor supervisor) {
+        return this.supervisorService.create(supervisor);
+    }
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Supervisor> findSupervisorById(@PathVariable long id) {
+        Supervisor supervisor =  this.supervisorService.getSupervisorById(id);
+        return new ResponseEntity<>(supervisor,HttpStatus.OK);
+    }
+    @GetMapping("/findRole/{id}")
+    public ResponseEntity<SupervisorRole> findRoleSupervisorById(@PathVariable long id) {
+        SupervisorRole supervisorrole =  this.supervisorService.getRoleSupervisorByid(id);
+        return new ResponseEntity<>(supervisorrole,HttpStatus.OK);
+    }
+}
